3a13c1639edd2122caf3ae8722980bb27a3b2518
AvSoft Technology Pvt Ltd <avsofttechnologypvtltd@gmail.com>
2025-09-14 13:27:00 +0000

    updated code

--- src/main/java/com/avsofthealthcare/config/SecurityConfig.java
+++ src/main/java/com/avsofthealthcare/config/SecurityConfig.java
@@ -113,7 +113,7 @@ public class SecurityConfig {
 	@Bean
 	public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
 		var config = new org.springframework.web.cors.CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:8080")); // adjust for frontend
+		config.setAllowedOrigins(List.of("http://localhost:8080","https://digihealth-374j.onrender.com")); // adjust for frontend
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
